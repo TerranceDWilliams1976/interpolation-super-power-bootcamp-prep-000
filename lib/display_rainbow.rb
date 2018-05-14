8b3edd65c8d2355da4134254f81d411542f3fe0a
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-14 00:50:57 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,6 +6,6 @@
 #   colors =
 # end
 
-def display_rainbow(color_list)
-  puts "R: #{color_list[0]}, O: #{color_list[1]}, Y: #{color_list[2]}, G: #{color_list[3]}, B: #{color_list[4]}, I: #{color_list[5]}, V: #{color_list[6]}"
+def display_rainbow(color_lists)
+  puts "R: #{color_lists[0]}, O: #{color_lists[1]}, Y: #{color_lists[2]}, G: #{color_lists[3]}, B: #{color_lists[4]}, I: #{color_lists[5]}, V: #{color_lists[6]}"
 end
